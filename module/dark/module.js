82373242ca4d3c1566b251efd528e35233224385
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-24 17:07:37 +0200

    Create module.js

---
+++ module/dark/module.js
@@ -0,0 +1,26 @@
+// Stripped down version of https://github.com/bufferhead-code/nightowl
+const css = document.createElement('style')
+css.innerHTML = `html {
+  /* Firefox fallback. */
+  background-color: #111;
+}
+body {
+  filter: invert(100%) hue-rotate(180deg);
+}
+/* Do not invert media (revert the invert). */
+img, video, iframe, .reverse {
+  filter: invert(100%) hue-rotate(180deg);
+}
+/* Improve contrast on icons. */
+.icon {
+  filter: invert(15%) hue-rotate(180deg);
+}
+/* Re-enable code block backgrounds. */
+pre {
+  filter: invert(6%);
+}
+/* Improve contrast on list item markers. */
+li::marker {
+  color: #666;
+}`;
+document.head.appendChild(css)
