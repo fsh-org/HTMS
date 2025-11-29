6479ed8511f7d5512c1de31e12509ba75baf4fe2
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-11-29 21:56:02 +0100

    Update ip module

--- module/ip/module.js
+++ module/ip/module.js
@@ -1,13 +1,9 @@
-function IP(type) {
+function IP() {
   return new Promise((resolve, reject) => {
-    fetch('https://api.fsh.plus/ip').then(async e=>{
-      e = await e.json();
-      if (!type) resolve(e.public);
-      if (type == "private") resolve(e.ip);
-      if (type == "public") resolve(e.public);
-      if (type == "all") resolve({public: e.public, private: e.ip});
-      resolve(e.ip)
-    })
+    fetch('https://api.fsh.plus/ip')
+      .then(e=>e.json())
+      .then(e=>resolve(e.ip))
+      .catch(_=>reject());
   });
 }
 
